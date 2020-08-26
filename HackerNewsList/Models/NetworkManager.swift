619a26b8d8127ab1212c013b6fba9682f414c832
chrizstvan <chris.stev30@gmail.com>
2020-08-26 11:52:35 +0700

    Implemet observable to Network manager Fetch data and implement to List

---
+++ HackerNewsList/Models/NetworkManager.swift
@@ -0,0 +1,40 @@
+//
+//  NetworkManager.swift
+//  HackerNewsList
+//
+//  Created by My Mac on 26/08/20.
+//  Copyright © 2020 Christian Stevanus. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager: ObservableObject {
+    
+    @Published var posts = [Post]()
+    
+    func fetchData() {
+        guard let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            if let safeData = data {
+                do {
+                    let results = try decoder.decode(Results.self, from: safeData)
+                    DispatchQueue.main.async {
+                        self.posts = results.hits
+                    }
+                } catch {
+                    print(error)
+                }
+                
+            }
+            
+        }
+        task.resume()
+    }
+}
---
+++ HackerNewsList/Views/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  HackerNewsList
+//
+//  Created by My Mac on 26/08/20.
+//  Copyright © 2020 Christian Stevanus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
+            }
+        .navigationBarTitle("Hacker News")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
+            .previewDisplayName("Pro Max 11")
+            
+            ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+            .previewDisplayName("iPhone SE")
+        }
+        
+    }
+}
+
+
